1d87579ac438462fc41f42afe67e6f2b7af234e1
Gang Fan <gang.fan@uconn.edu>
2016-08-23 09:04:42 -0400

    React Router Setting (Server Side)

--- app/components/app.js
+++ app/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {RouteHandler} from 'react-router';
-import Navbar from './Navbar';
-import Footer from ' ./Footer';
+// import Navbar from './Navbar';
+// import Footer from ' ./Footer';
 
 class App extends React.Component {
 	render() {
